b43d7a7a005319e42cc7a3adaecad7445f05ab91
wmt_php_amol <php7.webmob@gmail.com>
2019-10-08 15:55:51 +0530

    project setup

---
+++ README.md
@@ -0,0 +1,5 @@
+<!-- AUTO-GENERATED-CONTENT:START (STARTER) -->
+<h1 align="center">
+  hacktoberfest 2019
+</h1>
+<!-- AUTO-GENERATED-CONTENT:END -->
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>hacktoberfest2019</h1>
+  </Layout>
+)
+
+export default IndexPage
