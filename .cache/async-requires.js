1b966295d3bfc204e9c70d8527212f70805f276f
kamerk22 <kashyapk62@gmail.com>
2019-10-09 00:58:26 +0530

    fix .gitignore

--- .cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---cache-dev-404-page-js": () => import("/mnt/e/PROJECT_NEW/hacktoberfest2019/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("/mnt/e/PROJECT_NEW/hacktoberfest2019/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("/mnt/e/PROJECT_NEW/hacktoberfest2019/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
-}
-
