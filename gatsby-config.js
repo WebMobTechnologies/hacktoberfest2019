1fa1847a9fa6366221747b025ccb4bbcaa1cf8ce
kwaimind <danieel.reed@gmail.com>
2019-10-08 19:31:29 +0200

    adding google analytics (#8)

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,20 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ["UA-22140317-5"],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `hacktoberfest2019`,
