0886297fe31279769d830d192875fedb614ca0e4
Kashyap Merai <kamerk22@users.noreply.github.com>
2019-10-09 09:33:55 +0530

    Revert "[WIP] social share"

--- src/components/Line.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-const Line = () => <div className="line-br">
-    <span className="line-br-1">-</span>
-    <span className="line-br-2">-</span>
-</div>
-
-export default Line
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./Header"
+import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -17,11 +17,23 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <React.Fragment>
+    <>
+
       <Header siteTitle={data.site.siteMetadata.title} />
-      <main>{children}</main>
-    </React.Fragment>
-  ) 
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+        </footer>
+      </div>
+    </>
+  )
 }
 
 Layout.propTypes = {
