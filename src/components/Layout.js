df21d92a43ec2f2eecc81ba6670510eabb8dc5c4
php5-webmob <56337017+php5-webmob@users.noreply.github.com>
2019-10-11 14:07:32 +0530

    added schedule details :hammer: (#36)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,5 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 import CookieConsent from "react-cookie-consent"
 import Header from "./Header"
 import "./layout.css"
