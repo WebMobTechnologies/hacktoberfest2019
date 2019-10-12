4829fa9a9d55077d0fb99319ea8c9e1f2927be7e
php5-webmob <56337017+php5-webmob@users.noreply.github.com>
2019-10-12 19:05:21 +0530

    Update Contributors.js

--- src/components/Contributors.js
+++ src/components/Contributors.js
@@ -1,41 +1,72 @@
-import React from "react"
-import Line from "./Line"
-import { Row, Col } from "react-grid-system"
-import Contributor from '../../static/Contributors.json'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React from "react";
+import Line from "./Line";
+import { Row, Col } from "react-grid-system";
+import Contributor from "../../static/Contributors.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faTwitter,
-    faLinkedin,
-  faGithub,
-} from "@fortawesome/free-brands-svg-icons"
+  faTwitter,
+  faLinkedin,
+  faGithub
+} from "@fortawesome/free-brands-svg-icons";
 
 const Contributors = () => (
   <div>
     <Line />
     <h2 className="title is-2 is-spaced">Contributors</h2>
     <Row>
-        {Contributor.map(item => (
-            <Col xs={12} md={6} lg={4} key={Math.random()}>
-            <div className="box-projects">
+      {Contributor.map(item => (
+        <Col xs={12} md={6} lg={4} key={Math.random()}>
+          <div className="box-projects">
             <div className="img-desc">
-                <img src={item.image} alt="test" />   
+              <img src={item.image} alt="test" />
             </div>
             <div className="title-social">
-                <div className="title">
-                    <h3> {item.name}  </h3>
-                </div>
-                <div className="description">
-                    <a target="_blank" href={item.twitterUrl}><FontAwesomeIcon icon={faTwitter} />{item.twitterTitle}</a>
-                    <a target="_blank" ><FontAwesomeIcon icon={faGithub} />{item.githubTitle}</a>
-                    {/* <a target="_blank"><FontAwesomeIcon icon={faLinkedin} />{item.linkedinTitle}</a> */}
-                </div>
-            </div>
-            </div>       
-            </Col>
-         ))}        
-    </Row>
-    
-  </div>
-)
+              <div className="title">
+                <h3> {item.name} </h3>
+              </div>
+              <div className="description">
+                {item.twitter ? (
+                  <a
+                    target="_blank"
+                    href={`https://twitter.com/${item.twitter}`}
+                  >
+                    <FontAwesomeIcon icon={faTwitter} />
+                    {item.twitter}
+                  </a>
+                ) : (
+                  ""
+                )}
 
-export default Contributors
+                {item.github ? (
+                  <a
+                    target="_blank"
+                    href={`https://twitter.com/${item.github}`}
+                  >
+                    <FontAwesomeIcon icon={faGithub} />
+                    {item.github}
+                  </a>
+                ) : (
+                  ""
+                )}
+
+                {item.linkedin ? (
+                  <a
+                    target="_blank"
+                    href={`https://twitter.com/${item.linkedin}`}
+                  >
+                    <FontAwesomeIcon icon={faLinkedin} />
+                    {item.linkedin}
+                  </a>
+                ) : (
+                  ""
+                )}
+              </div>
+            </div>
+          </div>
+        </Col>
+      ))}
+    </Row>
+  </div>
+);
+
+export default Contributors;
