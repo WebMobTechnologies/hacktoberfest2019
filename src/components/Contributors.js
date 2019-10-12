7d7505abca04d0c4bf83b12841b2d45c5964a53c
krishna <krishnap@webmob.tech>
2019-10-12 12:45:41 +0530

    convert data to json file

--- src/components/Contributors.js
+++ src/components/Contributors.js
@@ -1,13 +1,12 @@
 import React from "react"
 import Line from "./Line"
-import { Container, Row, Col } from "react-grid-system"
-import test from '../../static/images/test.jpeg'
+import { Row, Col } from "react-grid-system"
+import Contributor from '../../static/Contributors.json'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
     faTwitter,
-  faInstagram,
+    faLinkedin,
   faGithub,
-  faStackOverflow,
 } from "@fortawesome/free-brands-svg-icons"
 
 const Contributors = () => (
@@ -15,53 +14,25 @@ const Contributors = () => (
     <Line />
     <h2 className="title is-2 is-spaced">Contributors</h2>
     <Row>
-        <Col xs={12} md={6} lg={4}>
+        {Contributor.map(item => (
+            <Col xs={12} md={6} lg={4} key={item.id}>
             <div className="box-projects">
-                <div className="img-desc">
-                    <img src={test} alt="test" />   
+            <div className="img-desc">
+                <img src={item.image} alt="test" />   
+            </div>
+            <div className="title-social">
+                <div className="title">
+                    <h3> {item.name} </h3>
                 </div>
-                <div className="title-social">
-                    <div className="title">
-                        <h3> Carthage </h3>
-                    </div>
-                    <div className="description">
-                        <a target="_blank"><FontAwesomeIcon icon={faTwitter} />@floristenhove</a>
-                        <a target="_blank"><FontAwesomeIcon icon={faGithub} />@floristenhove</a>
-                        <a target="_blank"><FontAwesomeIcon icon={faStackOverflow} />@floristenhove</a>
-                    </div>
+                <div className="description">
+                    <a target="_blank" href={item.twitterUrl}><FontAwesomeIcon icon={faTwitter} />{item.twitterTitle}</a>
+                    <a target="_blank" ><FontAwesomeIcon icon={faGithub} />{item.githubTitle}</a>
+                    {/* <a target="_blank"><FontAwesomeIcon icon={faLinkedin} />{item.linkedinTitle}</a> */}
                 </div>
             </div>
-        </Col>
-        <Col xs={12} md={6} lg={4}>
-            <div className="box-projects">
-                <div className="img-desc">
-                    <img src={test} alt="test" />   
-                </div>
-                <div className="title-social">
-                    <div className="title">
-                        <h3> Carthage </h3>
-                    </div>
-                    <div className="description">
-                        <a target="_blank"><FontAwesomeIcon icon={faInstagram} />@floristenhove</a>
-                    </div>
-                </div>
-            </div>
-        </Col>
-        <Col xs={12} md={6} lg={4}>
-            <div className="box-projects">
-                <div className="img-desc">
-                    <img src={test} alt="test" />   
-                </div>
-                <div className="title-social">
-                    <div className="title">
-                        <h3> Carthage </h3>
-                    </div>
-                    <div className="description">
-                        <a target="_blank"><FontAwesomeIcon icon={faInstagram} />@floristenhove</a>
-                    </div>
-                </div>
-            </div>
-        </Col>
+            </div>       
+            </Col>
+         ))}        
     </Row>
     
   </div>
